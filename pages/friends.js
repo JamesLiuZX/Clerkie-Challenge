04e918dd27a3b917d9927908ccadd0a37196ca24
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-04-20 09:30:06 +0800

    Define next.js pages for friends, app and index

---
+++ pages/friends.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import Friends from '../components/Friends';
+
+export default function FriendsPage() {
+  return (
+    <Layout title='Friends'>
+      <Friends />
+    </Layout>
+  );
+}
