14033602226fbb75aba46a8aefd70bcd6249b98a
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-04-20 13:40:49 +0800

    Finalise styling for home page container

--- components/Home.jsx
+++ components/Home.jsx
@@ -3,7 +3,6 @@ import styles from '../styles/Home.module.css';
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Home</h2>
       <div className={styles.separator}></div>
       <p className={styles.welcome}>Welcome to the Clerkie Challenge!</p>
     </div>
