39fdb74696ded0347ee5040a8288032e60c8e1da
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2023-04-20 13:14:29 +0800

    Update filter menu styling

--- components/FilterMenu.jsx
+++ components/FilterMenu.jsx
@@ -27,33 +27,34 @@ const FilterMenu = ({ onApply, onClose }) => {
 
   return (
     <div className={styles['filter-menu']}>
-      <div className={styles.header}>
+      <div className={styles['filter-menu-header']}>
         <span className={styles.clearAll} onClick={handleClearAll}>Clear all</span>
-        <span className={styles.title}>Filter</span>
+        <span className={styles['filter-menu-title']}>Filter</span>
         <button className={styles.closeButton} onClick={handleCloseMenu}>x</button>
       </div>
       <hr className={styles.separator} />
       <div className={styles['filter-options']}>
-        <h6>Friend Status</h6>
-        <label>
-          <input
-            type="checkbox"
-            value="Close Friends"
-            checked={selectedFilters.includes('Close Friends')}
-            onChange={handleCheckboxChange}
-          />{' '}
-          Close Friends
-        </label>
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            value="Super Close Friends"
-            checked={selectedFilters.includes('Super Close Friends')}
-            onChange={handleCheckboxChange}
-          />{' '}
-          Super Close Friends
-        </label>
+        <h6 className={styles.title}>Friend Status</h6>
+        <div className={styles['status-row']}>
+        <label>Close Friends</label>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          value="Close Friends"
+          checked={selectedFilters.includes('Close Friends')}
+          onChange={handleCheckboxChange}
+        />
+      </div>
+      <div className={styles['status-row']}>
+        <label>Super Close Friends</label>
+        <input
+          className={styles.checkbox}
+          type="checkbox"
+          value="Super Close Friends"
+          checked={selectedFilters.includes('Super Close Friends')}
+          onChange={handleCheckboxChange}
+        />
+      </div>
       </div>
       <button className={styles['apply-btn']} onClick={handleApplyClick}>
         Apply
